475d527a9fbf066e2a297d6478d1935189512d28
Rainbough Phillips <rainbough@gmail.com>
2013-09-05 18:03:44 -0500

    Put your commit message here

---
+++ class.rb
@@ -0,0 +1,12 @@
+5.send(:*, 5)
+"omg".send(:upcase)
+['a', 'b', 'c'].send(:at, 1)
+['a', 'b', 'c'].send(:insert, 2, 'o','n','0')
+{}.send(:size)
+{character: "Mario"}.send(:has_key?, :character)
+
+6 - 32
+{html: true, json: false}.keys
+"MakerSquare" * 6
+"Makersquare".split('a')
+['alpha', 'beta'].[](3)
